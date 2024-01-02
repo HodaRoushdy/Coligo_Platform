d25ea8e049fa0e9dc293d0ee6a07cef24eb0f157
noor el deen Mohammed Ibrahim <hodaroshdy106@gmail.com>
2024-01-02 17:00:26 +0200

    updating some spaces

--- src/pages/grade_book/index.tsx
+++ src/pages/grade_book/index.tsx
@@ -5,7 +5,7 @@ const GradeBookPage = () => {
   return (
     <div className="grid sm:grid-cols-1 xl:grid-cols-1">
       <div className="flex items-center justify-center h-screen bg-indigo-400">
-        <div className="bg-white p-10 shadow-2xl rounded-md border-indigo-900 outline-none">
+        <div className="bg-white p-8 shadow-2xl rounded-md border-indigo-900 outline-none">
           <h1 className="font-bold text-violet-950 text-3xl text-center">
             {t("gradebook.page.title")}
           </h1>
--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -5,7 +5,7 @@ const SchedulePage = () => {
   return (
     <div className="grid sm:grid-cols-1 xl:grid-cols-1">
       <div className="flex items-center justify-center h-screen bg-indigo-400">
-        <div className="bg-white p-10 shadow-2xl rounded-md border-indigo-900 outline-none">
+        <div className="bg-white p-8 shadow-2xl rounded-md border-indigo-900 outline-none">
           <h1 className="font-bold text-violet-950 text-3xl text-center">
             {t("schedule.page.title")}
           </h1>
